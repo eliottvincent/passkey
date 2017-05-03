040b4b858430aecb59953a9f49e7abc54f802814
Eliott Vincent <eliott.20100@gmail.com>
2017-05-03 14:07:02 +0200

    [EVi] fixed path for ressources

--- app/View/partials/head.php
+++ app/View/partials/head.php
@@ -15,19 +15,19 @@
 	<meta content="" name="author" />
 	<!-- BEGIN GLOBAL MANDATORY STYLES -->
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-	<link href="sources/View/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-	<link href="sources/View/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-	<link href="sources/View/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-	<link href="sources/View/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+	<link href="app/View/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="app/View/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+	<link href="app/View/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="app/View/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
 	<!-- END GLOBAL MANDATORY STYLES -->
 	<!-- BEGIN THEME GLOBAL STYLES -->
-	<link href="sources/View/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-	<link href="sources/View/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+	<link href="app/View/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+	<link href="app/View/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
 	<!-- END THEME GLOBAL STYLES -->
 	<!-- BEGIN THEME LAYOUT STYLES -->
-	<link href="sources/View/assets/layouts/layout/css/layout.min.css" rel="stylesheet" type="text/css" />
-	<link href="sources/View/assets/layouts/layout/css/themes/darkblue.min.css" rel="stylesheet" type="text/css" id="style_color" />
-	<link href="sources/View/assets/layouts/layout/css/custom.min.css" rel="stylesheet" type="text/css" />
+	<link href="app/View/assets/layouts/layout/css/layout.min.css" rel="stylesheet" type="text/css" />
+	<link href="app/View/assets/layouts/layout/css/themes/darkblue.min.css" rel="stylesheet" type="text/css" id="style_color" />
+	<link href="app/View/assets/layouts/layout/css/custom.min.css" rel="stylesheet" type="text/css" />
 	<!-- END THEME LAYOUT STYLES -->
 	<link rel="shortcut icon" href="../../../index.php" />
 </head>
