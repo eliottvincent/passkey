deab8520e07e58b674989e783df7efc25b09270e
Chloé Corfmat <contact@chloecorfmat.fr>
2017-05-04 14:36:58 +0200

    Update model User

--- app/Model/VO/UserVO.php
+++ app/Model/VO/UserVO.php
@@ -1,69 +1,75 @@
 <?php
+
+/**
+ * Created by PhpStorm.
+ * User: chloecorfmat
+ * Date: 04/05/2017
+ * Time: 14:25
+ */
 class UserVO
 {
+	private $identifier; // code apogee ou harpege
+	private $enssatPrimaryKey; // 32 bits
+	private $username;
+	private $name;
+	private $surname;
+	private $phone;
+	private $status; // etudiant, exterieur, personnel
+	private $email;
 
-    protected $ur1identifier; //code apogee ou harpege, selon statu
-    public function setUr1Identifier($id) {
-        $this->ur1identifier = $id;
-    }
-    public function getUr1Identifier() {
-        return $this->ur1identifier;
-    }
+	public function setIdentifier($identifier) {
+		$this->identifier = $identifier;
+	}
+	public function getIdentifier() {
+		return $this->identifier;
+	}
 
+	public function setEnssatPrimaryKey($id) {
+		$this->enssatPrimaryKey = $id;
+	}
+	public function getEnssatPrimaryKey() {
+		return $this->enssatPrimaryKey;
+	}
 
-    protected $enssatPrimaryKey; //32 bits
-    public function setEnssatPrimaryKey($id) {
-        $this->enssatPrimaryKey = $id;
-    }
-    public function getEnssatPrimaryKey() {
-        return $this->enssatPrimaryKey;
-    }
+	public function setUsername($username) {
+		$this->username = $username;
+	}
+	public function getUsername() {
+		return $this->username;
+	}
 
-    protected $username;
-    public function setUsername($username) {
-        $this->username = $username;
-    }
-    public function getUsername() {
-        return $this->username;
-    }
+	public function setName($name) {
+		$this->name = $name;
+	}
+	public function getName() {
+		return $this->name;
+	}
 
-    protected $name;
-    public function setName($name) {
-        $this->name = $name;
-    }
-    public function getName() {
-        return $this->name;
-    }
+	public function setSurname($surname) {
+		$this->surname = $surname;
+	}
+	public function getSurname() {
+		return $this->surname;
+	}
 
-    protected $surname;
-    public function setSurname($surname) {
-        $this->surname = $surname;
-    }
-    public function getSurname() {
-        return $this->surname;
-    }
+	public function setPhone($phone) {
+		$this->phone = $phone;
+	}
+	public function getPhone() {
+		return $this->phone;
+	}
 
-    protected $phone;
-    public function setPhone($phone) {
-        $this->phone = $phone;
-    }
-    public function getPhone() {
-        return $this->phone;
-    }
+	public function setStatus($status) {
+		$this->status = $status;
+	}
+	public function getStatus() {
+		return $this->status;
+	}
 
-    protected $status; //Etudiant, Exterieur, personel
-    public function setStatus($status) {
-        $this->status = $status;
-    }
-    public function getStatus() {
-        return $this->status;
-    }
-
-    protected $email;
-    public function setEmail($email) {
-        $this->email = $email;
-    }
-    public function getEmail() {
-        return $this->email;
-    }
+	public function setEmail($email) {
+		$this->email = $email;
+	}
+	public function getEmail() {
+		return $this->email;
+	}
 }
