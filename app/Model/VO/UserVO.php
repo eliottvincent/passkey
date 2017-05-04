580d3b016c6d25b02c9a8ebf0b4772539873708a
Chloé Corfmat <contact@chloecorfmat.fr>
2017-05-04 14:40:32 +0200

    Rearrange code

--- app/Model/VO/UserVO.php
+++ app/Model/VO/UserVO.php
@@ -8,7 +8,7 @@
  */
 class UserVO
 {
-	private $identifier; // code apogee ou harpege
+	private $ur1identifier; // code apogee ou harpege
 	private $enssatPrimaryKey; // 32 bits
 	private $username;
 	private $name;
@@ -17,59 +17,70 @@ class UserVO
 	private $status; // etudiant, exterieur, personnel
 	private $email;
 
-	public function setIdentifier($identifier) {
-		$this->identifier = $identifier;
-	}
-	public function getIdentifier() {
-		return $this->identifier;
+	// GETTER
+	public function getUr1identifier() {
+		return $this->ur1identifier;
 	}
 
-	public function setEnssatPrimaryKey($id) {
-		$this->enssatPrimaryKey = $id;
-	}
 	public function getEnssatPrimaryKey() {
 		return $this->enssatPrimaryKey;
 	}
 
-	public function setUsername($username) {
-		$this->username = $username;
-	}
 	public function getUsername() {
 		return $this->username;
 	}
 
-	public function setName($name) {
-		$this->name = $name;
-	}
 	public function getName() {
 		return $this->name;
 	}
 
-	public function setSurname($surname) {
-		$this->surname = $surname;
-	}
 	public function getSurname() {
 		return $this->surname;
 	}
 
-	public function setPhone($phone) {
-		$this->phone = $phone;
-	}
 	public function getPhone() {
 		return $this->phone;
+	}
+
+	public function getStatus() {
+		return $this->status;
 	}
 
 	public function setStatus($status) {
 		$this->status = $status;
 	}
-	public function getStatus() {
-		return $this->status;
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	// SETTER
+	public function setUr1identifier($ur1identifier) {
+		$this->ur1identifier = $ur1identifier;
+	}
+
+	public function setEnssatPrimaryKey($id) {
+		$this->enssatPrimaryKey = $id;
+	}
+
+	public function setUsername($username) {
+		$this->username = $username;
+	}
+
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	public function setSurname($surname) {
+		$this->surname = $surname;
+	}
+
+	public function setPhone($phone) {
+		$this->phone = $phone;
 	}
 
 	public function setEmail($email) {
 		$this->email = $email;
 	}
-	public function getEmail() {
-		return $this->email;
-	}
+
 }
