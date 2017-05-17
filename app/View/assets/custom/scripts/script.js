e91460a392e71ef3bd7a8443058201c8d42adc7f
Chloé Corfmat <contact@chloecorfmat.fr>
2017-05-17 15:13:19 +0200

    [CCO] Ajax about key delete

--- app/View/assets/custom/scripts/script.js
+++ app/View/assets/custom/scripts/script.js
@@ -24,7 +24,7 @@ function deleteKey() {
 			return new Promise(function (resolve, reject) {
 				$.ajax({
 					//url: "delete.php",
-					url: "/?action=deleteKeyAjax",
+					url: "delete.php",
 					type: "POST",
 					data: {
 						value: value,
@@ -35,45 +35,7 @@ function deleteKey() {
 							swal("Erreur !", "Merci de réessayer", "error");
 						} else {
 							swal("Fait !", "La clé a bien été supprimée", "success");
-							if (data.keys != null) {
-								var str = '';
-								for (var key in data.keys) {
-									console.log (data.keys[key].key_id);
-
-									var str_locks = '';
-									for (var i = 0; i < data.keys[key].key_locks.length; i++) {
-										str_locks += '<p>' + data.keys[key].key_locks[i] + '</p>';
-									}
-
-									var attr_number = '';
-									if (data.keys[key].key_number < 3) {
-										attr_number += "text-danger";
-									} else if (data.keys[key].key_number < 6) {
-										attr_number += "text-warning";
-									}
-
-									str += '<tr>' +
-										'<td>' + data.keys[key].key_id + '</td>' +
-										'<td>' + data.keys[key].key_name + '</td>' +
-										'<td>' + data.keys[key].key_type + '</td>' +
-										'<td>' + str_locks + '</td>' +
-										'<td><p class ="' + attr_number + '">' + data.keys[key].key_number + '</p></td>' +
-										'<td>' +
-										'<form action="./?action=updatekey" method="post">' +
-										'<input type="hidden" name="update" value="' + data.keys[key].key_id + '">' +
-										'<button type="submit" class="btn blue btn-sm">Modifier</button>' +
-										'</form>' +
-										'<input type="hidden" name="delete" value="' + data.keys[key].key_id + '">' +
-										'<button type="submit" class="btn red btn-sm btn-delete" value="' + data.keys[key].key_id + '">Supprimer</button>' +
-										'</td>'
-									'</tr>';
-								}
-								document.querySelector('tbody').innerHTML = str;
-								var btns = document.getElementsByClassName('btn-delete');
-								for (var i = 0; i < btns.length; i++) {
-									btns[i].addEventListener('click', deleteKey);
-								}
-							} else {
+							if (data.keys == null) {
 								// S'il n'y a plus de clés --> Affichage message d'erreur
 								var div = document.createElement('div');
 								div.setAttribute('class', 'alert alert-danger alert-dismissable');
@@ -87,7 +49,11 @@ function deleteKey() {
 								div.appendChild(button);
 								div.appendChild(p);
 								document.querySelector('.page-content').insertBefore(div, document.querySelector('.row'));
+
 								document.querySelector('tbody').innerHTML = '';
+ 							} else {
+								var tr = document.querySelector('#' + value);
+								document.querySelector('tbody').removeChild(tr);
 							}
 
 						}
