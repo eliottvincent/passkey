2e63d166597079794fdddaa6882a311827d9825a
Eliott Vincent <eliott.20100@gmail.com>
2017-06-01 23:46:37 +0200

    [EVi] added script for user deletion (WIP)

--- app/View/assets/custom/scripts/script.js
+++ app/View/assets/custom/scripts/script.js
@@ -6,16 +6,23 @@ window.addEventListener('load', initialiser);
 
 function initialiser(e) {
 	var kbtns = document.getElementsByClassName('btn-delete-k');
-	if (kbtns != null) {
+	if (kbtns !== null) {
 		for (var i = 0; i < kbtns.length; i++) {
 			kbtns[i].addEventListener('click', deleteKey);
 		}
 	}
 
 	var lbtns = document.getElementsByClassName('btn-delete-l');
-	if (lbtns != null) {
+	if (lbtns !== null) {
 		for (var i = 0; i < lbtns.length; i++) {
 			lbtns[i].addEventListener('click', deleteLock);
+		}
+	}
+
+	var ubtns = document.getElementsByClassName('btn-delete-u');
+	if (ubtns !== null) {
+		for (var i = 0; i < ubtns.length; i++) {
+			ubtns[i].addEventListener('click', deleteUser);
 		}
 	}
 }
@@ -36,15 +43,15 @@ function deleteKey() {
 					url: "/?action=deleteKeyAjax",
 					type: "POST",
 					data: {
-						value: keyId,
+						value: encodeURIComponent(keyId),
 					},
 					dataType: "json",
 					success: function (data) {
-						if( data.status == 'error' ) {
+						if( data.status === 'error' ) {
 							swal("Erreur !", "Merci de réessayer", "error");
 						} else {
 							swal("Fait !", "La clé a bien été supprimée", "success");
-							if (data.keys == null) {
+							if (data.keys === null) {
 								// S'il n'y a plus de clés --> Affichage message d'erreur
 								var div = document.createElement('div');
 								div.setAttribute('class', 'alert alert-danger alert-dismissable');
@@ -76,6 +83,7 @@ function deleteKey() {
 		allowOutsideClick: false
 	});
 }
+
 function deleteLock() {
 	var value = this.getAttribute('value');
 	swal({
@@ -92,15 +100,73 @@ function deleteLock() {
 					url: "/?action=deleteLockAjax",
 					type: "POST",
 					data: {
-						value: value,
+						value: encodeURIComponent(value),
 					},
 					dataType: "json",
 					success: function (data) {
-						if( data.status == 'error' ) {
+						if( data.status === 'error' ) {
 							swal("Erreur !", "Merci de réessayer", "error");
 						} else {
 							swal("Fait !", "Le canon a bien été supprimé", "success");
-							if (data.locks == null) {
+							if (data.locks === null) {
+								// S'il n'y a plus de canon --> Affichage message d'erreur
+								var div = document.createElement('div');
+								div.setAttribute('class', 'alert alert-danger alert-dismissable');
+								var button = document.createElement('button');
+								button.setAttribute('type', 'button');
+								button.setAttribute('class', 'close');
+								button.setAttribute('data-dismiss', 'alert');
+								button.setAttribute('aria-hidden', true);
+								var p = document.createElement('p');
+								p.innerHTML = 'Nous n\'avons aucun canon d\'enregistré.';
+								div.appendChild(button);
+								div.appendChild(p);
+								document.querySelector('.page-content').insertBefore(div, document.querySelector('.row'));
+
+								document.querySelector('tbody').innerHTML = '';
+							} else {
+								var tr = document.querySelector('#' + value);
+								document.querySelector('tbody').removeChild(tr);
+							}
+
+						}
+					},
+					error: function (xhr, ajaxOptions, thrownError) {
+						swal("Erreur !", "Merci de réessayer", "error");
+					}
+				});
+			})
+		},
+		allowOutsideClick: false
+	});
+}
+
+function deleteUser() {
+	var enssatPrimaryKey = this.getAttribute('value');
+	console.log(enssatPrimaryKey);
+	swal({
+		title: 'Êtes-vous sûr de vouloir supprimer cet utilisateur ?',
+		text: 'Cette action est irréversible',
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonText: 'Supprimer',
+		showLoaderOnConfirm: true,
+		preConfirm: function () {
+			return new Promise(function (resolve, reject) {
+				$.ajax({
+					//url: "delete.php",
+					url: "/?action=deleteUserAjax",
+					type: "POST",
+					data: {
+						value: encodeURIComponent(enssatPrimaryKey),
+					},
+					dataType: "json",
+					success: function (data) {
+						if( data.status === 'error' ) {
+							swal("Erreur !", "Merci de réessayer", "error");
+						} else {
+							swal("Fait !", "Le canon a bien été supprimé", "success");
+							if (data.locks === null) {
 								// S'il n'y a plus de canon --> Affichage message d'erreur
 								var div = document.createElement('div');
 								div.setAttribute('class', 'alert alert-danger alert-dismissable');
