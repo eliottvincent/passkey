5411364cfab1c35a15ad15c4282c52ff897867be
Eliott Vincent <eliott.20100@gmail.com>
2017-06-04 12:47:35 +0200

    [EVi] Lock creation is working

--- app/Controller/LockController.php
+++ app/Controller/LockController.php
@@ -13,6 +13,7 @@ class LockController {
 	 */
 	public function __construct() {
 		$this->_lockService = implementationLockService_Dummy::getInstance();
+		$this->_doorService = implementationDoorService_Dummy::getInstance();
 	}
 
 
@@ -83,52 +84,53 @@ class LockController {
 		echo $composite->render();
 	}
 
+
+	//================================================================================
+	// CREATE
+	//================================================================================
+
 	/**
 	 * To create a lock.
 	 */
 	public function create() {
-		if (!isset($_POST['lock_name']) && !isset($_POST['lock_door']) && !isset($_POST['lock_number'])) {
-			$doors = DoorController::getDoors();
-			if (!empty($doors)) {
-				// If we have no values, the form is displayed.
-				$this->displayForm(true);
-			} else {
-				$message['type'] = 'danger';
-				$message['message'] = 'Aucune porte n\'a été créée';
-				$this->displayForm(false, $message);
-			}
-		} elseif (empty($_POST['lock_name']) || empty($_POST['lock_door'])) {
-			// If we have not all values, error message display and form.
+
+		// if no values are posted
+		if (!isset($_POST['lock_name']) &&
+			!isset($_POST['lock_door'])) {
+
+			$this->displayForm();
+
+		}
+
+		// if some (but not all) values are posted
+		elseif (empty($_POST['lock_name']) ||
+			empty($_POST['lock_door'])) {
+
 			$m_type = "danger";
 			$m_message = "Toutes les valeurs nécessaires n'ont pas été trouvées. Merci de compléter tous les champs.";
 			$message['type'] = $m_type;
 			$message['message'] = $m_message;
-			$this->displayForm(true, $message);
-		} else {
-			// If we have all values.
 
 			$this->displayForm(array($message));
+		}
 
-			// Check unicity.
-			$exist = false;
-			$locks = $this::getLocks();
+		// if we have all values, we can create a lock
+		else {
 
-			if ($locks) {
-				foreach ($locks as $lock) {
-					if ($lock['lock_id'] == $id) {
-						$exist = true;
-					}
-				}
-			}
+			// id generation
+			$id = 'l_' . strtolower(str_replace(' ', '_', addslashes($_POST['lock_name'])));
+
+			// unicity check
+			$exist = $this->checkUnicity($id);
 
 			if (!$exist) {
-				$datas = array(
+				$lockToSave = array(
 					'lock_id' => $id,
 					'lock_name' => addslashes($_POST['lock_name']),
 					'lock_door' => addslashes($_POST['lock_door'])
 				);
 
-				$_SESSION['LOCKS'][] = $datas;
+				$this->saveLock($lockToSave);
 
 				$m_type = "success";
 				$m_message = "Le canon a bien été enregistré.";
@@ -137,6 +139,7 @@ class LockController {
 
 				$this->displayForm(array($message));
 			}
+			else {
 				$m_type = "danger";
 				$m_message = "Un canon avec le même nom existe déjà.";
 				$message['type'] = $m_type;
@@ -144,8 +147,6 @@ class LockController {
 
 				$this->displayForm(array($message));
 			}
-
-
 		}
 	}
 
@@ -154,102 +155,29 @@ class LockController {
 	 * @param $state boolean if some doors isset or not
 	 * @param null $message array The type and the text of the message
 	 */
-	public function displayForm($state, $message = null) {
-		if ($state) {
-			$doors = DoorController::getDoors();
-		} else {
-			$doors = null;
-		}
-		$composite = new CompositeView(true, 'Ajouter un canon', null, "lock");
+	public function displayForm($messages = null) {
 
-		if ($message != null && !empty($message['type']) && !empty($message['message'])) {
-			$message = new View("submit_message.html.twig", array("alert_type" => $message['type'] , "alert_message" => $message['message']));
-			$composite->attachContentView($message);
-		}
+		$doors = $this->getDoors();
 
-		$create_lock = new View("locks/create_lock.html.twig", array('doors' => $doors, 'previousUrl' => getPreviousUrl()));
-		$composite->attachContentView($create_lock);
-
-		echo $composite->render();
-	}
-
-	public function list() {
-		if (isset($_POST['delete']) && !empty($_POST['delete'])) {
-			$delete = $this->deleteLock(addslashes($_POST['delete']));
-			if ($delete) {
-				$message['type'] = 'success';
-				$message['message'] = 'Le canon a bien été supprimé.';
-				$messages[] = $message;
-
-				if(!isset($_SESSION['LOCKS'])) {
-					$message['type'] = 'danger';
-					$message['message'] = 'Nous n\'avons aucun canon d\'enregistré.';
-					$messages[] = $message;
-				}
-				if (!empty($this::getLocks())) {
-					$this->displayList(true, $messages);
-				} else {
-					$this->displayList(false, $messages);
-				}
-			} else {
-				$message['type'] = 'danger';
-				$message['message'] = 'Le canon n\'existe pas.';
-				$messages[] = $message;
-				if (!empty($this::getLocks())) {
-					$this->displayList(true, $messages);
-				} else {
-					$this->displayList(false, $messages);
-				}
-			}
-		} else {
-			$locks = $this::getLocks();
-			if (!empty($locks)) {
-				if (isset($_GET['update']) && $_GET['update'] == true) {
-					$alert['type'] = 'success';
-					$alert['message'] = 'Le canon a bien été modifié.';
-					$alerts[] = $alert;
-
-					$this->displayList(true, $alerts);
-				} else {
-					$this->displayList(true);
-				}
-
-			} else {
-				$alert['type'] = 'danger';
-				$alert['message'] = 'Nous n\'avons aucun canon d\'enregistré.';
-				$alerts[] = $alert;
-				$this->displayList(false, $alerts);
-			}
-		}
-	}
-
-	public function displayList($state, $messages = null) {
-		if ($state) {
-			$locks = LockController::getLocks();
-		} else {
-			$locks = null;
-		}
-		$composite = new CompositeView(
+		$compositeView = new CompositeView(
 			true,
-			'Liste des canons',
-			'Cette page permet de modifier et/ou supprimer des canons.',
-			"lock",
-			array("sweetAlert" => "https://cdn.jsdelivr.net/sweetalert2/6.6.2/sweetalert2.min.css"),
-			array("deleteLockScript" => "app/View/assets/custom/scripts/deleteLock.js",
-				"sweetAlert" => "https://cdn.jsdelivr.net/sweetalert2/6.6.2/sweetalert2.min.js"));
+			'Ajouter un canon',
+			null,
+			"lock");
 
 		if ($messages != null) {
 			foreach ($messages as $message) {
 				if (!empty($message['type']) && !empty($message['message'])) {
-					$submit_message = new View("submit_message.html.twig", array("alert_type" => $message['type'] , "alert_message" => $message['message']));
-					$composite->attachContentView($submit_message);
+					$message = new View("submit_message.html.twig", array("alert_type" => $message['type'] , "alert_message" => $message['message']));
+					$compositeView->attachContentView($message);
 				}
 			}
 		}
-		$list_locks = new View("locks/list_locks.html.twig", array('locks' => $locks));
-		$composite->attachContentView($list_locks);
 
-		echo $composite->render();
+		$create_lock = new View("locks/create_lock.html.twig", array('doors' => $doors, 'previousUrl' => getPreviousUrl()));
+		$compositeView->attachContentView($create_lock);
+
+		echo $compositeView->render();
 	}
 
 	public function deleteLock($id) {
@@ -324,30 +252,6 @@ class LockController {
 		echo $composite->render();
 	}
 
-	/**
-	 * Used to get all locks.
-	 * @return null
-	 */
-	public static function getLocks() {
-		if (isset($_SESSION['LOCKS'])) {
-			$locks = $_SESSION['LOCKS'];
-			return $locks;
-		}
-
-		return null;
-	}
-
-	public static function getLock($id) {
-		$locks = LockController::getLocks();
-
-		foreach ( $locks as $lock ) {
-			if ($lock['lock_id'] == $id) {
-				return $lock;
-			}
-		}
-
-		return false;
-	}
 
 	public function deleteLockAjax() {
 		session_start();
@@ -377,11 +281,38 @@ class LockController {
 	//================================================================================
 
 	/**
-	 * To get all doors
+	 * To get all locks
 	 * @return null
 	 */
 	public function getLocks() {
 
 		return $this->_lockService->getLocks();
 	}
+
+	/**
+	 * To get all doors
+	 * @return array
+	 */
+	public function getDoors() {
+
+		return $this->_doorService->getDoors();
+	}
+
+	/**
+	 * @param $lockToSave
+	 */
+	private function saveLock($lockToSave) {
+
+		$this->_lockService->saveLock($lockToSave);
+	}
+
+	/**
+	 * @param $id
+	 * @return bool
+	 */
+	private function checkUnicity($id) {
+
+		return $this->_lockService->checkUnicity($id);
+	}
+
 }
