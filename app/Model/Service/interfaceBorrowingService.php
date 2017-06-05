9e4680e10e5c0bd4755d185f982aad268903835a
Eliott Vincent <eliott.20100@gmail.com>
2017-06-05 14:55:16 +0200

    [EVi] added missing methods in BorrowingService interface

--- app/Model/Service/interfaceBorrowingService.php
+++ app/Model/Service/interfaceBorrowingService.php
@@ -5,13 +5,24 @@ interface interfaceBorrowingService
 {
 	//on emprunte toujours un trousseau
 
-	public function borrowKeychain($user, $keychain, DateTime $dueDate);
+	public function getBorrowings();
 
+	public function getBorrowing($id);
+
+	public function saveBorrowing($borrowingArray);
+
+	public function deleteBorrowing($id);
+
+	public function updateBorrowing($borrowingArray);
+
+	public function checkUnicity($id);
+
+	//=====
+	//=====
 	public function returnKeychain($borrowingId, $comment);
 
 	public function lostKeychain($borrowingId, $comment);
 
-	public function getBorrowings();
 }
 
 ?>
