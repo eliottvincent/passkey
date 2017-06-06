c9ad7b053f909ef3eb99fab559f339ffa83a9cb0
Eliott Vincent <eliott.20100@gmail.com>
2017-06-06 13:59:42 +0200

    [EVi] changes for new CompositeView signature

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -3,9 +3,16 @@
 	class DashboardController
 	{
 		public function displayDash() {
-			$composite = new CompositeView(true, "Dashboard", null, null);
+			$composite = new CompositeView(
+				true,
+				"Dashboard",
+				null,
+				"dashboard",
+				null,
+				array("waypointsScript" => "app/View/assets/global/plugins/counterup/jquery.waypoints.min.js",
+					"counterupScript" => "app/View/assets/global/plugins/counterup/jquery.counterup.min.js"));
 
-			$displayDash = new View(null, null, 'dashboard/dashboard.html.twig');
+			$displayDash = new View('dashboard/dashboard.html.twig');
 			$composite->attachContentView($displayDash);
 
 			echo $composite->render();
