2276d926a8fc86bdb041737c332697db575ffa8c
Eliott Vincent <eliott.20100@gmail.com>
2017-06-07 10:03:35 +0200

    [EVi] added basic Tracking DAO

---
+++ app/Model/DAO/interfaceTrackingDAO.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: eliottvincent
+ * Date: 07/06/2017
+ * Time: 09:58
+ */
+interface interfaceTrackingDAO
+{
+
+	public static function getInstance();
+
+	// Retrieves all users currently in the database.
+	public function getTrackings();
+}
