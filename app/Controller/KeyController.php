d5fbc461cfa66a87a6ae9ab6044bb7877ee22767
Eliott Vincent <eliott.20100@gmail.com>
2017-06-08 18:57:09 +0200

    [EVi] multiple locks in key while update

--- app/Controller/KeyController.php
+++ app/Controller/KeyController.php
@@ -236,7 +236,7 @@ class KeyController {
 				'key_id' => $_POST['key_id'],
 				'key_name' => addslashes($_POST['key_name']),
 				'key_type' => addslashes($_POST['key_type']),
-				'key_locks' => addslashes($_POST['key_locks']),
+				'key_locks' => $_POST['key_locks'],
 				'key_copies' => addslashes($_POST['key_copies']));
 
 			if ($this->updateKey($keyToUpdate) == false) {
