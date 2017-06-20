18bb46c13c5af146e670bf5fd4ec0a02c2d71b0a
Unknown <basile.bruhat@etudiant.univ-rennes1.fr>
2017-06-20 14:32:50 +0200

    [BBr]Send ID user/key to PDF

--- app/Controller/PDFController.php
+++ app/Controller/PDFController.php
@@ -19,8 +19,8 @@ class PDFController
 	 */
 	public function __construct()
 	{
-		/*$this->_keychainService = implementationKeyChainService_Dummy::getInstance();
-		$this->_userService = implementationUserService_Dummy::getInstance();*/
+		$this->_keychainService = implementationKeyChainService_Dummy::getInstance();
+		$this->_userService = implementationUserService_Dummy::getInstance();
 	}
 	public function test($html){
 		$dompdf = new DOMPDF();  //if you use namespaces you may use new \DOMPDF()
@@ -29,10 +29,6 @@ class PDFController
 		$dompdf->stream("sample.pdf", array("Attachment"=>0));
 	}
 
-	private function getUser($enssatPrimaryKey) {
-
-		return $this->_userService->getUser($enssatPrimaryKey);
-	}
 
 	public function getBorrowingById($borrowingId)
 	{
@@ -50,9 +46,10 @@ class PDFController
 		$keyid = $_GET['keyname'];
 		$userid = $_GET['user'];
 
-		/*$keyname = $this->getBorrowingById($keyid);
-		$username = $this->getUser($userid);*/
+		$key = $this->getBorrowingById($keyid);
+		$user = $this->getUser($userid);
 
+		$username = $user->getUsername();
 
 		/*$this->test("<div><h1>L'application PassKey vous remercie de votre emprunt</h1>
 			<p> <L'utilisateur ". $userid .
@@ -63,9 +60,10 @@ class PDFController
 			" a emprunté la clé : " . $keyid.
 			" ! </p></div>");
 
-		$this->test("<div><body> <h1>L'emprunt demandé a bien été réalisé</h1> <table> <tr > <td > L'utilisateur [User] a emprunté la clé : [clé] </td></tr><tr> <td > Pour confirmer l'emprunt, merci de signer ce reçu </td></tr><tr> <td> La scolarité : </td><td style=\"width:300px; height:100px;\"> [User] : </td></tr></table> </body> </html><style>tr /* Toutes les cellules des tableaux... */table{border-collapse: collapse;}table /* Mettre une bordure sur les td ET les th */{border: 1px solid black;}</style></div>");
-
-
 	}
 
+	private function getUser($enssatPrimaryKey) {
+
+		return $this->_userService->getUser($enssatPrimaryKey);
+	}
 }
