251f0998945aafb096e1bfbaa6648e247c56d9f0
Eliott Vincent <eliott.20100@gmail.com>
2017-06-26 19:15:19 +0200

    [EVï] dropdown select is working for Key creation

--- app/Controller/KeyController.php
+++ app/Controller/KeyController.php
@@ -175,8 +175,16 @@ class KeyController {
 			'Ajouter une clé',
 			null,
 			"keys",
-			null,
-			array("chooseKey" => "app/View/assets/custom/scripts/chooseKey.js"));
+			array(
+				"select2minCss" => "app/View/assets/custom/scripts/select2/css/select2.min.css",
+				"select2bootstrap" => "app/View/assets/custom/scripts/select2/css/select2-bootstrap.min.css"
+			),
+			array(
+				"chooseKey" => "app/View/assets/custom/scripts/chooseKey.js",
+				"select2min" => "app/View/assets/custom/scripts/select2/js/select2.full.min.js",
+				"customselect2" => "app/View/assets/custom/scripts/components-select2.js"
+			)
+		);
 
 		if ($messages != null) {
 			foreach ($messages as $message) {
