f7f4e1b56663e2b3326a2933fbb6152b7c3ff2bd
Eliott Vincent <eliott.20100@gmail.com>
2017-06-27 02:14:50 +0200

    [EVi] added dropdown when updating a Door

--- app/Controller/DoorController.php
+++ app/Controller/DoorController.php
@@ -257,7 +257,17 @@ class DoorController {
 			true,
 			"Mettre à jour une porte",
 			null,
-			"doors");
+			"doors",
+			array(
+				"select2minCss" => "app/View/assets/custom/scripts/select2/css/select2.min.css",
+				"select2bootstrap" => "app/View/assets/custom/scripts/select2/css/select2-bootstrap.min.css"
+			),
+			array(
+				"chooseKey" => "app/View/assets/custom/scripts/chooseKey.js",
+				"select2min" => "app/View/assets/custom/scripts/select2/js/select2.full.min.js",
+				"customselect2" => "app/View/assets/custom/scripts/components-select2.js"
+			)
+		);
 
 		if ($messages != null) {
 
