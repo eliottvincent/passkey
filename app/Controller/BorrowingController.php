96d5dfa72da669b7b506f5f83ad3e9129393576c
Eliott Vincent <eliott.20100@gmail.com>
2017-06-27 09:55:35 +0200

    [EVi] added dropdown while creating a Borrowing

--- app/Controller/BorrowingController.php
+++ app/Controller/BorrowingController.php
@@ -171,7 +171,17 @@ class BorrowingController {
 			true,
 			'Ajouter un emprunt',
 			null,
-			"borrowings");
+			"borrowings",
+			array(
+				"select2minCss" => "app/View/assets/custom/scripts/select2/css/select2.min.css",
+				"select2bootstrap" => "app/View/assets/custom/scripts/select2/css/select2-bootstrap.min.css"
+			),
+			array(
+				"chooseKey" => "app/View/assets/custom/scripts/chooseKey.js",
+				"select2min" => "app/View/assets/custom/scripts/select2/js/select2.full.min.js",
+				"customselect2" => "app/View/assets/custom/scripts/components-select2.js"
+			)
+		);
 
 		if ($messages != null) {
 			foreach ($messages as $message) {
